1cd161b53aa6a14c08e620d0cf0bf47300d41191
knebadze <nebo.nebadze98@gmail.com>
2023-01-29 16:31:03 +0400

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
 // app.js
-require('./bootstrap');
+import('./bootstrap');
 
-import {createApp} from 'vue'
+import {createApp} from 'vue/dist/vue.esm-bundler'
 
 // import App from './App.vue'
 import logIn from "./component/modal/log_in.vue";
 import howWork from './component/how_work.vue'
 const app = createApp({})
+console.log(app);
 
 app.component('log-in', logIn);
 app.component('how-work', howWork)
-app.mount("#app")
+app.mount("#appPage")
