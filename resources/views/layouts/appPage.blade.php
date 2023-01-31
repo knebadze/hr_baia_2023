215be494a67bf9c4cf8385c2d9ecda6527e7f3ec
knebadze <nebo.nebadze98@gmail.com>
2023-01-31 15:52:50 +0400

    register

--- resources/views/layouts/appPage.blade.php
+++ resources/views/layouts/appPage.blade.php
@@ -57,7 +57,7 @@
         @yield('content')
 
         @include('inc.footer')
-        
+
         <!-- BUTTON TOP START -->
 		<button class="scroltop"><span class="fa fa-angle-up  relative" id="btn-vibrate"></span></button>
 
@@ -92,55 +92,117 @@
                                     <!--Signup Candidate Content-->
                                     <div class="tab-pane fade show active" id="sign-candidate">
                                         <div class="row">
+                                            <form method="POST" action="{{ route('register') }}">
+                                                @csrf
 
-                                            <div class="col-lg-12">
-                                                <div class="form-group mb-3">
-                                                    <input name="username" type="text" required="" class="form-control" placeholder="Usearname*">
-                                                </div>
-                                            </div>
+                                                <div class="row mb-3">
+                                                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
-                                            <div class="col-lg-12">
-                                                <div class="form-group mb-3">
-                                                    <input name="email" type="text" class="form-control" required="" placeholder="Password*">
-                                                </div>
-                                            </div>
+                                                    <div class="col-md-6">
+                                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                            <div class="col-lg-12">
-                                                <div class="form-group mb-3">
-                                                    <input name="phone" type="text" class="form-control" required="" placeholder="Email*">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-lg-12">
-                                                <div class="form-group mb-3">
-                                                    <input name="phone" type="text" class="form-control" required="" placeholder="Phone*">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-lg-12">
-                                                <div class="form-group mb-3">
-                                                    <div class=" form-check">
-                                                        <input type="checkbox" class="form-check-input" id="agree1">
-                                                        <label class="form-check-label" for="agree1">I agree to the <a href="javascript:;">Terms and conditions</a></label>
-                                                        <p>Already registered?
-                                                            <button class="twm-backto-login" data-bs-target="#sign_up_popup2" data-bs-toggle="modal" data-bs-dismiss="modal">Log in here</button>
-                                                        </p>
+                                                        @error('name')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="col-md-12">
-                                                <button type="submit" class="site-button">Sign Up</button>
-                                            </div>
 
+                                                <div class="row mb-3">
+                                                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                                        @error('email')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+                                                <div class="row mb-3">
+                                                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+                                                <div class="row mb-3">
+                                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                                    </div>
+                                                </div>
+
+                                                <div class="row mb-0">
+                                                    <div class="col-md-6 offset-md-4">
+                                                        <button type="submit" class="btn btn-primary">
+                                                            {{ __('Register') }}
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            {{-- <form method="POST" action="{{ route('register') }}">
+                                                @csrf
+                                                <div class="col-lg-12">
+                                                    <div class="form-group mb-3">
+                                                        <input name="name" type="text"  class="form-control" placeholder="Usearname*">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-12">
+                                                    <div class="form-group mb-3">
+                                                        <input name="email" type="text" class="form-control"  placeholder="Password*">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-12">
+                                                    <div class="form-group mb-3">
+                                                        <input name="phone" type="text" class="form-control"  placeholder="Email*">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-12">
+                                                    <div class="form-group mb-3">
+                                                        <input name="phone" type="text" class="form-control"  placeholder="Phone*">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-12">
+                                                    <div class="form-group mb-3">
+                                                        <div class=" form-check">
+                                                            <input type="checkbox" class="form-check-input" id="agree1">
+                                                            <label class="form-check-label" for="agree1">I agree to the <a href="javascript:;">Terms and conditions</a></label>
+                                                            <p>Already registered?
+                                                                <button class="twm-backto-login" data-bs-target="#sign_up_popup2" data-bs-toggle="modal" data-bs-dismiss="modal">Log in here</button>
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <button type="submit" class="site-button">Sign Up</button>
+                                                </div>
+                                            </form> --}}
                                         </div>
                                     </div>
                                     <!--Signup Employer Content-->
-                                    <div class="tab-pane fade" id="sign-Employer">
+                                    {{-- <div class="tab-pane fade" id="sign-Employer">
                                         <div class="row">
 
                                             <div class="col-lg-12">
                                                 <div class="form-group mb-3">
-                                                    <input name="username" type="text" required="" class="form-control" placeholder="Usearname*">
+                                                    <input name="username" type="text" class="form-control" placeholder="Usearname*">
                                                 </div>
                                             </div>
 
@@ -178,7 +240,7 @@
                                             </div>
 
                                         </div>
-                                    </div>
+                                    </div> --}}
 
                                     </div>
                                 </div>
