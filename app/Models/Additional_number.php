a05be9e9082f0fe3afed3b1b02a51b04b8185c19
knebadze <nebo.nebadze98@gmail.com>
2023-02-06 15:26:30 +0400

    database

---
+++ app/Models/Additional_number.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Additional_number extends Model
+{
+    use HasFactory;
+}
