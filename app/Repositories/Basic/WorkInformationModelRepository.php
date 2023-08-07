b873bc62a97577549c1a4a2e979818d45dbf167d
knebadze <nebo.nebadze98@gmail.com>
2023-08-07 22:30:32 +0400

    start work information

--- app/Repositories/Basic/WorkInformationModelRepository.php
+++ app/Repositories/Basic/WorkInformationModelRepository.php
@@ -32,19 +32,19 @@ class WorkInformationModelRepository
             $candidateRecommendation = [];
         }
 
-        if (Candidate::where('user_id', $auth_id)->exists() && CandidateFamilyWorkSkill::where('candidate_id', $user->candidate->id)->exists()) {
+        // if (Candidate::where('user_id', $auth_id)->exists() && CandidateFamilyWorkSkill::where('candidate_id', $user->candidate->id)->exists()) {
 
-            $candidateFamilyWorkSkill = CandidateFamilyWorkSkill::where('candidate_id', $user->candidate->id)
-                            ->join('skills', 'candidate_family_work_skills.skill_id', 'skills.id')->get();
-            foreach ($candidateFamilyWorkSkill as $key => $value) {
-                $familyWorkedSelectedArr[] = $value->category_id;
-            }
+        //     $candidateFamilyWorkSkill = CandidateFamilyWorkSkill::where('candidate_id', $user->candidate->id)
+        //                     ->join('skills', 'candidate_family_work_skills.skill_id', 'skills.id')->get();
+        //     foreach ($candidateFamilyWorkSkill as $key => $value) {
+        //         $familyWorkedSelectedArr[] = $value->category_id;
+        //     }
 
-            $familyWorkedSelected = Category::whereIn('id', $familyWorkedSelectedArr)->get();
-        }else{
-            $familyWorkedSelected = [];
-            $candidateFamilyWorkSkill = [];
-        }
+        //     $familyWorkedSelected = Category::whereIn('id', $familyWorkedSelectedArr)->get();
+        // }else{
+        //     $familyWorkedSelected = [];
+        //     $candidateFamilyWorkSkill = [];
+        // }
 
         if (Candidate::where('user_id', $auth_id)->exists() && FamilyWorkExperience::where('candidate_id', $user->candidate->id)->exists()) {
             $familyWorkExperience = FamilyWorkExperience::where('candidate_id', $user->candidate->id)->with('workExperience')->with('longest')->with('noReason')->with('hasExperience')->first()->toArray();
@@ -57,9 +57,9 @@ class WorkInformationModelRepository
             'workInformation' => $workInformation,
             'getWorkInformation' => $getWorkInformation,
             'candidateRecommendation' => $candidateRecommendation,
-            'candidateFamilyWorkSkill' => $candidateFamilyWorkSkill,
+            // 'candidateFamilyWorkSkill' => $candidateFamilyWorkSkill,
             'familyWorkExperience' => $familyWorkExperience,
-            'familyWorkedSelected' => $familyWorkedSelected,
+            // 'familyWorkedSelected' => $familyWorkedSelected,
         ];
         return $date;
     }
