4e890c06954539b85d4b7193641044a692a83564
knebadze <nebo.nebadze98@gmail.com>
2023-12-19 14:00:57 +0400

    add deposit sms

--- app/Services/SendDailySmsService.php
+++ app/Services/SendDailySmsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Vacancy;
 use Illuminate\Support\Carbon;
 use App\Models\QualifyingCandidate;
 use App\Events\SmsNotificationEvent;
@@ -23,7 +24,48 @@ class SendDailySmsService
         if ($probationPeriod) {
             $this->sendSms();
         }
+        $depositReminder = Vacancy::where('status_id', 3)
+            ->whereHas('deposit', function ($query) {
+                return $query->whereNot('must_be_enrolled_employer', 0)->orWhereNot('must_be_enrolled_candidate', 0);
+            })->get();
 
+        $whereEmployerDateMoreThanCurrent = $depositReminder->filter(function ($vacancy) use ($currentDate) {
+            return $vacancy->deposit->must_be_enrolled_employer_date > $currentDate;
+        });
+
+        $whereEmployerDateLessThanCurrent = $depositReminder->filter(function ($vacancy) use ($currentDate) {
+            return $vacancy->deposit->must_be_enrolled_employer_date <= $currentDate;
+        });
+
+        $whereCandidateDateMoreThanCurrent = $depositReminder->filter(function ($vacancy) use ($currentDate) {
+            return $vacancy->deposit->must_be_enrolled_candidate_date > $currentDate;
+        });
+
+        $whereCandidateDateLessThanCurrent = $depositReminder->filter(function ($vacancy) use ($currentDate) {
+            return $vacancy->deposit->must_be_enrolled_candidate_date <= $currentDate;
+        });
+
+        if($whereEmployerDateMoreThanCurrent){
+            $data = [
+                'to' => $depositReminder->employer->number,
+                'name' => $depositReminder->employer->name_ka,
+                'money' => $depositReminder->deposit->must_be_enrolled_employer,
+                'date' => $depositReminder->deposit->must_be_enrolled_employer_date
+            ];
+            $name = 'deposit_enrolled_candidate_or_employer';
+        }
+        if($whereCandidateDateMoreThanCurrent){
+            $data = [
+                'to' => $depositReminder->employer->number,
+                'name' => $depositReminder->employer->name_ka,
+                'money' => $depositReminder->deposit->must_be_enrolled_employer,
+                'date' => $depositReminder->deposit->must_be_enrolled_employer_date
+            ];
+            $name = 'deposit_enrolled_candidate_or_employer';
+        }
+    }
+
+    function hrBlock(){
         
     }
 
