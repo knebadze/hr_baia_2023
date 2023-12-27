0a53ef7f8b5bc57b5ea583a0ad98a0bd1e2063b4
knebadze <nebo.nebadze98@gmail.com>
2023-12-27 16:13:49 +0400

    sms service

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -14,24 +14,22 @@ class  SmsService
         $service_id = config('services.smsservicege.service_id');
         $to = $phone_number;
         $encodedMessage = urlencode($message);
-        $opts = [
-            'http' => [
-                'method' => 'GET',
-                'header' => "mDwJD@ejqs",
-            ],
-        ];
-        $context = stream_context_create($opts);
-        $generatedUrl = "{$apiUrl}?username={$username}&password={$password}&client_id={$client_id}&service_id={$service_id}&to=+995{$to}&text={$encodedMessage}&utf=8";
-        // dd($generatedUrl);
-        $file = file_get_contents($generatedUrl,
-		false, $context);
-        // dd($file);
-
-
+        // $opts = [
+        //     'http' => [
+        //         'method' => 'GET',
+        //         'header' => "mDwJD@ejqs",
+        //     ],
+        // ];
+        // $context = stream_context_create($opts);
         // $generatedUrl = "{$apiUrl}?username={$username}&password={$password}&client_id={$client_id}&service_id={$service_id}&to=+995{$to}&text={$encodedMessage}&utf=8";
-        // $response = Http::withHeaders([
-        //     'header' => 'mDwJD@ejqs',
-        // ])->get($generatedUrl);
+        // // dd($generatedUrl);
+        // $file = file_get_contents($generatedUrl,
+		// false, $context);
+        // // dd($file);
+
+
+        $generatedUrl = "{$apiUrl}?username={$username}&password={$password}&client_id={$client_id}&service_id={$service_id}&to=+995{$to}&text={$encodedMessage}&utf=8";
+        $response = Http::get($generatedUrl);
         // $encodedMessage = urlencode($message);
 
         // // dd($encodedMessage);
@@ -57,6 +55,6 @@ class  SmsService
         //     $errorResponse = $response->json();
         //     dd('error',$errorResponse);
         // }
-        // return $response->status();
+        return $response->status();
     }
 }
