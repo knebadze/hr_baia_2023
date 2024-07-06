8ea73f6c3765adafb72cce20de6d94f816eb029d
knebadze <nebo.nebadze98@gmail.com>
2024-07-06 15:21:42 +0400

    refactor - avatar upload

--- app/Http/Controllers/MyprofileController.php
+++ app/Http/Controllers/MyprofileController.php
@@ -51,30 +51,31 @@ class MyprofileController extends Controller
         try {
             $user = User::findOrFail($request->id);
 
-            if ($request->hasFile('avatar')) {
-                $oldAvatar = $user->avatar;
-                $avatar = $request->file('avatar');
-
-                $image = Image::make($avatar)->fit(300, 300);
-                $filePath = 'user_avatar/' . $avatar->hashName();
-
-                // Store image in public disk
-                if (Storage::disk('public')->put($filePath, (string) $image->encode())) {
-                    $user->avatar = $filePath;
-
-                    // Delete old avatar if it exists and is not default
-                    if ($oldAvatar && !in_array($oldAvatar, ['user_avatar/default_male.jpg', 'user_avatar/default_female.jpg'])) {
-                        Storage::disk('public')->delete($oldAvatar);
-                    }
-
-                    $user->save();
-                    return response()->json($user);
-                } else {
-                    Log::error('Failed to store image: Storage::put() failed.');
-                    return response()->json(['error' => 'Failed to store image.'], 500);
-                }
-            } else {
+            if (!$request->hasFile('avatar')) {
                 return response()->json(['error' => 'No file uploaded.'], 400);
+                
+            }
+
+            $avatar = $request->file('avatar');
+            $oldAvatar = $user->avatar;
+
+            $image = Image::make($avatar)->fit(300, 300);
+            $filePath = 'user_avatar/' . $avatar->hashName();
+
+            // Store image in public disk
+            if (Storage::disk('public')->put($filePath, (string) $image->encode())) {
+                $user->avatar = $filePath;
+
+                // Delete old avatar if it exists and is not default
+                if ($oldAvatar && !in_array($oldAvatar, ['user_avatar/default_male.jpg', 'user_avatar/default_female.jpg'])) {
+                    Storage::disk('public')->delete($oldAvatar);
+                }
+
+                $user->save();
+                return response()->json($user);
+            } else {
+                Log::error('Failed to store image: Storage::put() failed.');
+                return response()->json(['error' => 'Failed to store image.'], 500);
             }
         } catch (\Exception $e) {
             Log::error('Failed to upload image: ' . $e->getMessage());
