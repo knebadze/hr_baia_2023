504a8b75c76279f3260ad01a5fa8db8437fae96b
knebadze <nebo.nebadze98@gmail.com>
2024-08-03 11:13:05 +0400

    fix - active log causer bug

---
+++ app/Http/Middleware/SetActivityLogAuthDriver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+
+class SetActivityLogAuthDriver
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::guard('staff')->check()) {
+            Config::set('activitylog.default_auth_driver', 'staff');
+        } else {
+            Config::set('activitylog.default_auth_driver', 'web');
+        }
+
+        return $next($request);
+    }
+}
